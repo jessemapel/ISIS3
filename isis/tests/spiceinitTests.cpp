795e16a3f082aea9a1b02a42b230deeb147710b3
Jesse Mapel <jmapel@usgs.gov>
2020-01-03 16:30:49 -0700

    Added PvlGroup comp and default

--- isis/tests/spiceinitTests.cpp
+++ isis/tests/spiceinitTests.cpp
@@ -46,9 +46,10 @@ class spiceinitTestCube : public ::testing::Test {
   }
 };
 
+
 TEST_F(spiceinitTestCube, PredictAndReconCK) {
 
-  std::istringstream clementineLabelStrm(R"(
+  std::istringstream labelStrm(R"(
     Object = IsisCube
       Object = Core
         StartByte   = 65537
@@ -100,29 +101,13 @@ TEST_F(spiceinitTestCube, PredictAndReconCK) {
         NaifFrameCode = -40022
       End_Group
     End_Object
-
-    Object = Label
-      Bytes = 65536
-    End_Object
-
-    Object = History
-      Name      = IsisCube
-      StartByte = 176129
-      Bytes     = 456
-    End_Object
-
-    Object = OriginalLabel
-      Name      = IsisCube
-      StartByte = 176585
-      Bytes     = 5409
-    End_Object
   End
   )");
 
-  Pvl clementineLabel;
-  clementineLabelStrm >> clementineLabel;
+  Pvl label;
+  labelStrm >> label;
 
-  createCube(clementineLabel);
+  createCube(label);
 
   spiceinitOptions options;
   options.ckrecon = true;
@@ -138,11 +123,14 @@ TEST_F(spiceinitTestCube, PredictAndReconCK) {
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, instrumentPointing[0], "$Clementine1/kernels/ck/clem_2mn.bck");
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, instrumentPointing[1], "$Clementine1/kernels/ck/clem_5sc.bck");
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, instrumentPointing[2], "$clementine1/kernels/fk/clem_v12.tf");
+  ASSERT_TRUE(kernels.hasKeyword("InstrumentPointingQuality"));
+  EXPECT_PRED_FORMAT2(AssertQStringsEqual, kernels["InstrumentPointingQuality"][0], "Reconstructed");
 }
+
 
 TEST_F(spiceinitTestCube, CkConfigFile) {
 
-  std::istringstream crismLabelStrm(R"(
+  std::istringstream labelStrm(R"(
     Object = IsisCube
       Object = Core
         StartByte   = 65537
@@ -203,29 +191,13 @@ TEST_F(spiceinitTestCube, CkConfigFile) {
         NaifIkCode = -74017
       End_Group
     End_Object
-
-    Object = Label
-      Bytes = 65536
-    End_Object
-
-    Object = History
-      Name      = IsisCube
-      StartByte = 176129
-      Bytes     = 456
-    End_Object
-
-    Object = OriginalLabel
-      Name      = IsisCube
-      StartByte = 176585
-      Bytes     = 5409
-    End_Object
   End
   )");
 
-  Pvl crismLabel;
-  crismLabelStrm >> crismLabel;
+  Pvl label;
+  labelStrm >> label;
 
-  createCube(crismLabel);
+  createCube(label);
 
   spiceinit(&testCube);
 
@@ -237,4 +209,120 @@ TEST_F(spiceinitTestCube, CkConfigFile) {
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, instrumentPointing[1], "$mro/kernels/ck/mro_crm_psp_110223_101128.bc");
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, instrumentPointing[2], "$mro/kernels/ck/mro_sc_psp_110222_110228.bc");
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, instrumentPointing[3], "$mro/kernels/fk/mro_v15.tf");
+}
+
+
+TEST_F(spiceinitTestCube, Default) {
+
+  std::istringstream labelStrm(R"(
+    Object = IsisCube
+      Object = Core
+        StartByte   = 65537
+        Format      = Tile
+        TileSamples = 128
+        TileLines   = 128
+
+        Group = Dimensions
+          Samples = 1204
+          Lines   = 1056
+          Bands   = 1
+        End_Group
+
+        Group = Pixels
+          Type       = UnsignedByte
+          ByteOrder  = Lsb
+          Base       = 0.0
+          Multiplier = 1.0
+        End_Group
+      End_Object
+
+      Group = Instrument
+        SpacecraftName       = VIKING_ORBITER_1
+        InstrumentId         = VISUAL_IMAGING_SUBSYSTEM_CAMERA_B
+        TargetName           = MARS
+        StartTime            = 1977-07-09T20:05:51
+        ExposureDuration     = 0.008480 <seconds>
+        SpacecraftClockCount = 33322515
+        FloodModeId          = ON
+        GainModeId           = HIGH
+        OffsetModeId         = ON
+      End_Group
+
+      Group = Archive
+        DataSetId       = VO1/VO2-M-VIS-2-EDR-V2.0
+        ProductId       = 387A06
+        MissonPhaseName = EXTENDED_MISSION
+        ImageNumber     = 33322515
+        OrbitNumber     = 387
+      End_Group
+
+      Group = BandBin
+        FilterName = CLEAR
+        FilterId   = 4
+      End_Group
+
+      Group = Reseaus
+        Line     = (5, 6, 8, 9, 10, 11, 12, 13, 14, 14, 15, 133, 134, 135, 137,
+                    138, 139, 140, 141, 141, 142, 143, 144, 263, 264, 266, 267,
+                    268, 269, 269, 270, 271, 272, 273, 393, 393, 395, 396, 397,
+                    398, 399, 399, 400, 401, 402, 403, 523, 524, 525, 526, 527,
+                    527, 528, 529, 530, 530, 532, 652, 652, 654, 655, 656, 657,
+                    657, 658, 659, 660, 661, 662, 781, 783, 784, 785, 786, 787,
+                    788, 788, 789, 790, 791, 911, 912, 913, 914, 915, 916, 917,
+                    918, 918, 919, 920, 921, 1040, 1041, 1043, 1044, 1045, 1045,
+                    1046, 1047, 1047, 1048, 1050)
+        Sample   = (24, 142, 259, 375, 491, 607, 723, 839, 954, 1070, 1185, 24,
+                    84, 201, 317, 433, 549, 665, 780, 896, 1011, 1127, 1183, 25,
+                    142, 259, 375, 492, 607, 722, 838, 953, 1068, 1183, 25, 84,
+                    201, 317, 433, 549, 665, 779, 895, 1010, 1125, 1182, 25, 143,
+                    259, 375, 491, 607, 722, 837, 952, 1067, 1182, 25, 84, 201,
+                    317, 433, 548, 664, 779, 894, 1009, 1124, 1181, 25, 142, 258,
+                    374, 490, 605, 720, 835, 951, 1066, 1180, 24, 83, 200, 316,
+                    431, 547, 662, 776, 892, 1007, 1122, 1179, 23, 140, 257, 373,
+                    488, 603, 718, 833, 948, 1063, 1179)
+        Type     = (1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+                    5, 6, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 4, 5, 5, 5, 5, 5, 5, 5,
+                    5, 5, 5, 6, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 4, 5, 5, 5, 5, 5,
+                    5, 5, 5, 5, 5, 6, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 4, 5, 5, 5,
+                    5, 5, 5, 5, 5, 5, 5, 6, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6)
+        Valid    = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        Template = $viking1/reseaus/vo1.visb.template.cub
+        Status   = Nominal
+      End_Group
+    End_Object
+  End
+  )");
+
+  Pvl label;
+  labelStrm >> label;
+
+  createCube(label);
+
+  spiceinit(&testCube);
+
+  PvlGroup kernels = testCube.group("Kernels");
+
+  EXPECT_TRUE(kernels.hasKeyword("InstrumentPointing"));
+  EXPECT_TRUE(kernels.hasKeyword("LeapSecond"));
+  EXPECT_TRUE(kernels.hasKeyword("TargetAttitudeShape"));
+  EXPECT_TRUE(kernels.hasKeyword("TargetPosition"));
+  EXPECT_TRUE(kernels.hasKeyword("InstrumentPointing"));
+  EXPECT_TRUE(kernels.hasKeyword("Instrument"));
+  EXPECT_TRUE(kernels.hasKeyword("SpacecraftClock"));
+  EXPECT_TRUE(kernels.hasKeyword("InstrumentPosition"));
+  EXPECT_TRUE(kernels.hasKeyword("InstrumentAddendum"));
+  EXPECT_TRUE(kernels.hasKeyword("ShapeModel"));
+  EXPECT_TRUE(kernels.hasKeyword("InstrumentPositionQuality"));
+  EXPECT_TRUE(kernels.hasKeyword("InstrumentPointingQuality"));
+  EXPECT_TRUE(kernels.hasKeyword("CameraVersion"));
+
+  spiceinit(&testCube);
+
+  PvlGroup secondKernels = testCube.group("Kernels");
+
+  EXPECT_PRED_FORMAT2(AssertPvlGroupEqual, secondKernels, kernels);
 }
