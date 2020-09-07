d9f5b6a3508ef2a9dd1619fa25fd94eb055448cb
Jesse Mapel <jmapel@usgs.gov>
2020-09-07 15:13:30 -0700

    Ported to callable

---
+++ isis/src/base/apps/equalizer/equalizer.h
@@ -0,0 +1,11 @@
+#ifndef equalizer_h
+#define equalizer_h
+
+#include "Pvl.h"
+#include "UserInterface.h"
+
+namespace Isis {
+  extern void equalizer(UserInterface &ui, Pvl *log=nullptr);
+}
+
+#endif
